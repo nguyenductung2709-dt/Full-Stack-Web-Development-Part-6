0f4d4435cbdee8629311701600f5cb0b0123774b
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-02 18:42:08 +0200

    6.10

--- anecdotes-redux/src/reducers/filterReducer.js
+++ anecdotes-redux/src/reducers/filterReducer.js
@@ -1,18 +1,14 @@
-const filterReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_FILTER':
-        return [...state, action.payload];
-      default:
-        return state;
-    }
-  };
-  
-  export const filterChange = word => {
-    return {
-      type: 'SET_FILTER',
-      payload: word,
-    };
-  };
-  
-  export default filterReducer;
-  
+import { createSlice } from '@reduxjs/toolkit';
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: [],
+  reducers: {
+    filterChange: (state, action) => {
+      return [...state, action.payload];
+    },
+  },
+});
+
+export const { filterChange } = filterSlice.actions;
+export default filterSlice.reducer;
