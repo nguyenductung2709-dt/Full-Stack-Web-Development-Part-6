5b55c44207382751ee0513a4c36f53aa5813cbaa
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-03 18:53:46 +0200

    6.20

--- anecdotes-reactquery/src/App.jsx
+++ anecdotes-reactquery/src/App.jsx
@@ -1,19 +1,36 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+
+
 
 const App = () => {
+  const queryClient = useQueryClient()
+
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: false
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isError) {
+    return <span>anecdote service not available due to problems in server</span>
+  }
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
