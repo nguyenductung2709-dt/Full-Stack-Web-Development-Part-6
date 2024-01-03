84c7a71fbcd86b6694a0c06008aab7ea5589baf5
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-03 05:22:09 +0200

    6.14

--- anecdotes-redux/src/App.jsx
+++ anecdotes-redux/src/App.jsx
@@ -3,12 +3,22 @@ import AnecdoteForm from './components/AnecdoteForm.jsx';
 import AnecdoteList from './components/AnecdoteList.jsx';
 import Filter from './components/Filter.jsx';
 import Notification from './components/Notification.jsx';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const notification = useSelector(state => state.notification);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [])
+
 
   return (
+    
     <div>
       <h2>Anecdotes</h2>
       {notification && notification.message && <Notification />} 
